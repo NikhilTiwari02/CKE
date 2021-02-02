d4b3b59b63c8a4725ff12405b2b3e17c01c2dfa6
Nikhil Tiwari <71125250+NikhilTiwari02@users.noreply.github.com>
2021-02-02 23:20:39 +0530

    Add files via upload

---
+++ pp001.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{    //pp001: Write a program to input a number and check it to be even or odd using if statement.
+    int n;
+    printf("ENTER THE NUMBER :");
+    scanf("%d",&n);
+    if(n%2==0)
+    printf("Number is even");
+    else
+        printf("Number is odd");
+    return 0;
+
+
+
+}
